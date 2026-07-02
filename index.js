6edbe32148bf62b2cfa77b557cf6c57265039d01
Raju-ahamed <rajuahamed7320@gmail.com>
2026-07-02 14:24:38 +0600

    2nd repo

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+
 const users = [
     { id: 1, name: 'sabana mam', email: 'sabana@gamil.com' },
-    { id: 1, name: 'sabnur mam', email: 'sabnur@gamil.com' },
-    { id: 1, name: 'salman mam', email: 'salman@gamil.com' }
+    { id: 2, name: 'sabnur mam', email: 'sabnur@gamil.com' },
+    { id: 3, name: 'salman mam', email: 'salman@gamil.com' }
 ]
 
 app.get("/", (req, res) => {
@@ -14,7 +18,10 @@ app.get("/", (req, res) => {
 app.get("/users", (req, res) => {
     res.send(users);
 })
-
+app.post("/users", (req, res) => {
+    console.log("api hiting")
+    console.log(req.body);
+})
 app.listen(port, () => {
     console.log(`server log ${port}`);
 
