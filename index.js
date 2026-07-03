9ffda3eb88b3a8eade6a62e77ab1be89939aa383
Raju-ahamed <rajuahamed7320@gmail.com>
2026-07-03 11:42:57 +0600

    3rd repo

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use(express.json())
 
 const users = [
     { id: 1, name: 'sabana mam', email: 'sabana@gamil.com' },
@@ -21,6 +22,10 @@ app.get("/users", (req, res) => {
 app.post("/users", (req, res) => {
     console.log("api hiting")
     console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser)
 })
 app.listen(port, () => {
     console.log(`server log ${port}`);
